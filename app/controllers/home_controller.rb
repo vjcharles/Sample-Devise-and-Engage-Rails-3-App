55d502e3b887e24d27e9bfe14a6981f4c7f709ec
Vincent Charles <vjcharles@gmail.com>
2011-05-03 23:12:43 -0700

    Clean app with Devise and Janrain Engage (rpx) user authentication

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    @message = "hello"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EngageMe::Application.config.secret_token = '52061bf1b8bc44321af7b9843710be6fa30b70f01db754058d5a0d4372f315042cfdae0727af4f79ffb2f0e403e9c1f3726dde121237e6cba2ecbd3b8f6d996a'
